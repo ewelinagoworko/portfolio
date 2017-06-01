a9a5a2cbe060f359f4adf337178fd22206733ceb
Ewelina Goworko <ewelina.goworko@gmail.com>
2017-06-01 21:04:26 +0200

    commit

--- js/custom-main.js
+++ js/custom-main.js
@@ -23,4 +23,71 @@ $(function(){
 	
 });
 
+$ (function(){
+	
+	
+	var slider = $('#slider'); 
+	var slideShow = $('.slide-show'); 
+	var slideCount = $('.single-slide').length; 
+	var slideWidth = 100/slideCount;
+	var slideIndex = 0;
+	
+	console.log(slideCount);
+	
+	
+	$('.slide-show').css('width', slideCount*100 + '%'); 
+	
+	
+	slideShow.find('.single-slide').each(function(index) {
+								
+			$(this).css({
+				'width' : slideWidth + '%',
+				'margin-left': slideWidth * index + '%'
+			});
+		 });
+			
+	//PREV
+		
+	$('.prev-slide').click(function(e){
+		e.preventDefault();
+		slide(slideIndex - 1);	
+		//console.log(newSlideIndex);
+	});
+	
+	//NEXT
+		
+	$('.next-slide').click(function(e) {
+		e.preventDefault();
+		slide(slideIndex + 1);	
+		//console.log(newSlideIndex);
+	});
+
+	//SLIDE Function
+		
+ 	function slide (newSlideIndex){ 
+		console.log(newSlideIndex);
+	 	if (newSlideIndex < 0 || newSlideIndex >= slideCount) {
+			return; 
+		}
+		
+		var slideCaption = $('.slider-caption').eq(newSlideIndex);
+		var slideText = $('p').eq(newSlideIndex);
+		var bomb = $('.fa-bomb').eq(newSlideIndex);
+		slideCaption.hide();
+		slideText.hide();
+		bomb.hide();
+		
+		var marginLeft = (newSlideIndex * (-100)) + '%';
+		
+		$('.slide-show').animate({'margin-left': marginLeft}, 800, indexNapis);
+		
+		function indexNapis() {
+			slideIndex = newSlideIndex;
+			slideCaption.fadeIn('slow');
+			slideText.fadeIn('slow');
+			bomb.fadeIn('slow');
+		}
+	};
+
+});
 
